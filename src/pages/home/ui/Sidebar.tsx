9f0d1fb854dcd9fd32d0f5d10bfad7e6983e3a03
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 00:53:42 +0300

    Refactor: Changed name import to default

--- src/pages/home/ui/Sidebar.tsx
+++ src/pages/home/ui/Sidebar.tsx
@@ -1,4 +1,8 @@
 import { type FC } from 'react';
 
 // TODO: This component will be implementing soon [PR #7]
-export const Sidebar: FC = () => null;
+const Sidebar: FC = () => null;
+
+Sidebar.displayName = 'Sidebar';
+
+export default Sidebar;
