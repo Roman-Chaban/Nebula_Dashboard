a63eb21fffca4eda06615e4853b6d68f2a600263
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 00:27:38 +0300

    Feat: Added Home component, mock component props

---
+++ src/pages/home/ui/Home.tsx
@@ -0,0 +1,9 @@
+import { type FC } from 'react';
+
+// TODO: This component will be adding for HomePage
+
+// export type HomeProps = {};
+
+export const Home: FC = () => {
+  return <section></section>;
+};
