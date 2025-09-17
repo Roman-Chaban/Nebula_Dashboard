5f9d924fea82e89521d527e29fa9cdaf28da5ba8
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 00:33:12 +0300

    Feat: Add props to Home & added displayName

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -1,9 +1,15 @@
-import { type FC } from 'react';
+import { type ReactNode, type FC } from 'react';
 
 // TODO: This component will be adding for HomePage
 
-// export type HomeProps = {};
-
-export const Home: FC = () => {
-  return <section></section>;
+export type HomeProps = {
+  children: ReactNode;
 };
+
+export const Home: FC<HomeProps> = ({ children }) => {
+  return <section>{children}</section>;
+};
+
+Home.displayName = 'Home';
+
+export default Home;
