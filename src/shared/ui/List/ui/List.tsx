4697a203a876295da13e4ef7ecdb0e705ccec7a9
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 19:57:13 +0300

    Feat: Added reusable List component

---
+++ src/shared/ui/List/ui/List.tsx
@@ -0,0 +1,21 @@
+import { ListItemKey, ListProps } from '@/entities/ui/list/model/types';
+
+export const List = <T extends { id: ListItemKey }>({
+  tag: Tag,
+  renderList,
+  className = '',
+  itemClassName = '',
+  renderItem,
+  getItemKey = (item) => item.id,
+  ...props
+}: ListProps<T>) => {
+  return (
+    <Tag className={className} {...props}>
+      {renderList.map((item, index) => (
+        <li key={getItemKey(item)} className={itemClassName}>
+          {renderItem(item, index)}
+        </li>
+      ))}
+    </Tag>
+  );
+};
