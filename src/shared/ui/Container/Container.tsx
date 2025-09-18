316681af57d93d540a08bf2bf257432d39e57cab
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 15:50:20 +0300

    Feat: Added reusable Container component with styled, and other props

---
+++ src/shared/ui/Container/Container.tsx
@@ -0,0 +1,17 @@
+import { type FC } from 'react';
+import { ContainerProps } from '@/entities/ui/container/model/types';
+import { parseSize } from '@/shared/lib/ui/parseSize';
+
+export const Container: FC<ContainerProps> = ({
+  children,
+  htmlTag: Tag = 'div',
+  width,
+  height,
+  className,
+}) => {
+  return (
+    <Tag className={className} style={{ width: parseSize(width), height: parseSize(height) }}>
+      {children}
+    </Tag>
+  );
+};
