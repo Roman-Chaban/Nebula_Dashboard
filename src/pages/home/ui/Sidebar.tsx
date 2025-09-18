b8b2a9a4a26efbc6cc736ab314750af28c8a0a43
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-18 15:44:15 +0300

    Refactor: Moved Sidebar component to widgets directory to clear FSD architecture, replaced import

--- src/pages/home/ui/Sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { type FC } from 'react';
-
-// TODO: This component will be implementing soon [PR #7]
-const Sidebar: FC = () => null;
-
-Sidebar.displayName = 'Sidebar';
-
-export default Sidebar;
---
+++ src/widgets/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import { type FC } from 'react';
+import { SidebarLogo } from '@/widgets/ui/Sidebar/SidebarLogo';
+import { Container } from '@/shared/ui/Container/Container';
+
+// TODO: This component will be implementing soon [PR #7]
+const Sidebar: FC = () => {
+  return (
+    <Container
+      htmlTag="article"
+      width="100%"
+      height="100%"
+      className="flex min-h-[100vh] max-w-72 justify-center bg-white"
+    >
+      <SidebarLogo />
+    </Container>
+  );
+};
+
+Sidebar.displayName = 'Sidebar';
+
+export default Sidebar;
