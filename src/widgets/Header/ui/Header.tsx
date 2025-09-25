89615efb08a475ecd5f841e15010e654b482383b
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-25 18:18:33 +0300

    feat: Refactor Header component and add HeaderInfo, HeaderInput, and HeaderInputRightSide components

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,13 +1,19 @@
 import { type FC } from 'react';
 import { HeaderProps } from '@/widgets/Header/model/types';
+import { HeaderInfo } from '@/widgets/Header/ui/HeaderInfo';
+import { HeaderInput } from '@/widgets/Header/ui/HeaderInput';
+import { Container } from '@/shared/ui/Container/Container';
+import { HeaderInputRightSide } from './HeaderInputRightSide';
 
 export const Header: FC<HeaderProps> = ({ title, subtitle }) => {
   return (
-    <header>
-      <div>
-        <span>{subtitle}</span>
-        <h1>{title}</h1>
-      </div>
+    <header className="w-full">
+      <Container className="flex w-full items-center justify-between pt-12 pr-[22px] pb-7 pl-[30px]">
+        <HeaderInfo title={title} subtitle={subtitle} />
+        <HeaderInput>
+          <HeaderInputRightSide />
+        </HeaderInput>
+      </Container>
     </header>
   );
 };
