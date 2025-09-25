f24e7b005c40f119ac531a882fde9668faa34fe4
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-25 18:32:16 +0300

    Refactor: Enhance SidebarNavItem to use dynamic active link styling

--- src/widgets/Sidebar/ui/Sidebar/SidebarNavItem.tsx
+++ src/widgets/Sidebar/ui/Sidebar/SidebarNavItem.tsx
@@ -1,16 +1,27 @@
+'use client';
+
 import { type FC } from 'react';
 
 import { SidebarNavItemProps } from '@/widgets/Sidebar/model/types';
 import { Container } from '@/shared/ui/Container/Container';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const SidebarNavItem: FC<SidebarNavItemProps> = ({ item }) => {
   const { icon, label, href } = item;
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
   return (
     <Container htmlTag="div" className="flex min-h-[36px] items-center gap-3">
       {icon}
-      <Link href={href} className="font-bold text-[var(--color-light-icon)]">
+      <Link
+        href={href}
+        className={`font-bold ${
+          isActive ? 'text-[var(--color-primary)]' : 'text-[var(--color-light-icon)]'
+        }`}
+      >
         {label}
       </Link>
     </Container>
