99c42b166db885e6018c7d1f84f7103f037180ea
Roman Chaban <roman.chaban1001@gmail.com>
2025-09-25 18:19:44 +0300

    feat: Refactor HomePage component to use Container for layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Home from '@/pages/home/ui/Home';
 import { Header } from '@/widgets/Header/ui/Header';
+import { Container } from '@/shared/ui/Container/Container';
 
 export default function HomePage() {
   return (
-    <>
-      <Header title="Main Dashboard" subtitle="Pages / Dashboard" />
+    <Container htmlTag="div" className="flex">
       <Home />
-    </>
+      <Header title="Main Dashboard" subtitle="Pages / Dashboard" />
+    </Container>
   );
 }
