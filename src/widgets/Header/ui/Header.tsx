0a18a5cfd79c79f9cd5356732928cd2a7f7821ad
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-01 16:42:36 +0300

    Refactor: Simplify Header and HeaderInfo components by consolidating imports and updating structure

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -2,13 +2,12 @@ import { type FC } from 'react';
 import { HeaderProps } from '@/widgets/Header/model/types';
 import { HeaderInfo } from '@/widgets/Header/ui/HeaderInfo';
 import { HeaderInput } from '@/widgets/Header/ui/HeaderInput';
-import { Container } from '@/shared/ui/Container/Container';
+import { Container, LanguageSwitcher } from '@/shared/ui/index';
 import { HeaderInputRightSide } from './HeaderInputRightSide';
-import { LanguageSwitcher } from '@/shared/ui/LanguageSwitcher/LanguageSwitcher';
 
 export const Header: FC<HeaderProps> = ({ title, subtitle }) => {
   return (
-    <header className="flex h-max w-full">
+    <header className="col-span-12 w-full">
       <Container className="flex w-full items-center justify-between">
         <HeaderInfo title={title} subtitle={subtitle} />
         <HeaderInput>
--- src/widgets/Header/ui/HeaderInfo.tsx
+++ src/widgets/Header/ui/HeaderInfo.tsx
@@ -1,15 +1,20 @@
-import { FC } from 'react';
+import { type FC } from 'react';
 import { HeaderInfoProps } from '@/widgets/Header/model/types';
+import { Container } from '@/shared/ui/Container/Container';
+import { Heading } from '@/shared/ui/Heading/Heading';
 
 export const HeaderInfo: FC<HeaderInfoProps> = ({ title, subtitle }) => {
   return (
-    <div className="flex flex-col">
+    <Container className="flex flex-col">
       <span className="leading-extra text-secondary inline-block text-[14px] font-medium">
         {subtitle}
       </span>
-      <h1 className="text-primary leading-medium text-[34px] font-bold tracking-[-0.02em]">
+      <Heading
+        level={1}
+        className="text-primary leading-medium text-[34px] font-bold tracking-[-0.02em]"
+      >
         {title}
-      </h1>
-    </div>
+      </Heading>
+    </Container>
   );
 };
