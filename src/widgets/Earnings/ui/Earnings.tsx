9edeb7d13f2b60eeabc547305d44ded6d74b5f42
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-01 18:37:22 +0300

    Feat: Implement Earing component and integrate it into Earnings component

--- src/widgets/Earnings/ui/Earnings.tsx
+++ src/widgets/Earnings/ui/Earnings.tsx
@@ -1,5 +1,19 @@
 import { type FC } from 'react';
 
+import { Earing } from '@/widgets/Earnings/index';
+import { Container, List } from '@/shared/ui';
+import { earingItems } from '@/widgets/Earnings/model/config';
+
 export const Earnings: FC = () => {
-  return <div>Earnings</div>;
+  return (
+    <Container className="w-full">
+      <List
+        itemClassName="w-full max-w-[248px]"
+        className="flex w-full flex-wrap items-center gap-5"
+        getItemKey={(earing) => earing.id}
+        renderList={earingItems}
+        renderItem={(earing) => <Earing key={earing.id} earing={earing} />}
+      />
+    </Container>
+  );
 };
