7defd4ce496ec0b1fd5244cc87abe12f574f9250
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-02 17:19:22 +0300

    Feat: Add metadata for Dashboard Page

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,5 +1,13 @@
+import { type Metadata } from 'next';
+
 import { Container, PageLayout } from '@/shared/ui/index';
 import { Earnings } from '@/widgets/Earnings/ui/Earnings';
+
+export const metadata: Metadata = {
+  title: 'Nebula | Dashboard Page',
+  description: 'Dashboard page showing your earnings and performance metrics.',
+  keywords: ['dashboard', 'earnings', 'analytics', 'Nebula'],
+};
 
 export default function DashboardPage() {
   return (
