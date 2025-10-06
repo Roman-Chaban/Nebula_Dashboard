02c87fb32eafec19979d582689e913a308b9571d
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-06 18:25:51 +0300

    Feat: Add MonthChart and WeeklyChart components for structured chart display

---
+++ src/widgets/Charts/ui/MonthChart.tsx
@@ -0,0 +1,6 @@
+import { type FC } from 'react';
+import { ChartsBox } from '@/widgets/Charts/index';
+
+export const MonthChart: FC = () => {
+  return <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px]">MonthChart</ChartsBox>;
+};
---
+++ src/widgets/Charts/ui/WeeklyChart.tsx
@@ -0,0 +1,8 @@
+import { type FC } from 'react';
+import { ChartsBox } from '@/widgets/Charts/index';
+
+export const WeeklyChart: FC = () => {
+  return (
+    <ChartsBox paddingClass="pt-[29px] pr-[28.74px] pb-[27px] pl-[31px]">WeeklyChart</ChartsBox>
+  );
+};
