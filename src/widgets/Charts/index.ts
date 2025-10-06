782a20e842ebd077ddfa9e54b818f7c29d76e49b
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-06 18:25:38 +0300

    Feat: Add index file to export Chart components for improved accessibility

---
+++ src/widgets/Charts/index.ts
@@ -0,0 +1,3 @@
+export { MonthChart } from '@/widgets/Charts/ui/MonthChart';
+export { WeeklyChart } from '@/widgets/Charts/ui/WeeklyChart';
+export { ChartsBox } from '@/widgets/Charts/ui/ChartsBox';
