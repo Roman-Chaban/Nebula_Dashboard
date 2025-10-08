bd98a5297facf2fcc4ddc936fbedff73827078af
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 19:00:06 +0300

    Feat: update MonthChartMain to include LineChartWithSlider component

--- src/widgets/Charts/ui/MonthChart/MonthChartMain.tsx
+++ src/widgets/Charts/ui/MonthChart/MonthChartMain.tsx
@@ -2,11 +2,13 @@ import { type FC } from 'react';
 
 import { Container } from '@/shared/ui';
 import { MonthChartInformation } from '@/widgets/Charts/index';
+import LineChartWithSlider from './LineChartWithSlider';
 
 export const MonthChartMain: FC = () => {
   return (
-    <Container htmlTag="div" className="">
+    <Container htmlTag="div" className="flex">
       <MonthChartInformation />
+      <LineChartWithSlider />
     </Container>
   );
 };
