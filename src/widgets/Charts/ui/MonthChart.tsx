9b0879eed0d471206190fe1b8925a165ae146a12
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 17:08:38 +0300

    Feat: Update MonthChart and WeeklyChart to include localized titles and extraBlock in ChartHeader

--- src/widgets/Charts/ui/MonthChart.tsx
+++ src/widgets/Charts/ui/MonthChart.tsx
@@ -1,10 +1,18 @@
+'use client';
+
 import { type FC } from 'react';
+
 import { ChartsBox, ChartHeader } from '@/widgets/Charts/index';
+import { CalendarIcon } from '@/shared/ui/Icons/Icons';
+import { useTranslation } from 'react-i18next';
 
 export const MonthChart: FC = () => {
+  const { t } = useTranslation();
   return (
     <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px]">
-      <ChartHeader />
+      <ChartHeader
+        extraBlock={{ label: t('CHARTS.WEEKLY_CHART.extra_title'), icon: <CalendarIcon /> }}
+      />
     </ChartsBox>
   );
 };
