51cfd3b0d672c77bd4ca7ad0e1b9016e4b1bd66d
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 16:34:46 +0300

    Feat: Add ChartHeader component for structured chart display

---
+++ src/widgets/Charts/ui/ChartHeader.tsx
@@ -0,0 +1,7 @@
+import { type FC } from 'react';
+import { Container } from '@/shared/ui';
+import { ChartHeaderProps } from '@/widgets/Charts/model/types';
+
+export const ChartHeader: FC<ChartHeaderProps> = () => {
+  return <Container>Header</Container>;
+};
