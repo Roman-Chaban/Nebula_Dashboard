478db1275a75eb4478732ea4a2a439b0232a3d79
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 16:34:39 +0300

    Feat: Update MonthChart and WeeklyChart to include ChartHeader for improved structure

--- src/widgets/Charts/ui/MonthChart.tsx
+++ src/widgets/Charts/ui/MonthChart.tsx
@@ -1,6 +1,10 @@
 import { type FC } from 'react';
-import { ChartsBox } from '@/widgets/Charts/index';
+import { ChartsBox, ChartHeader } from '@/widgets/Charts/index';
 
 export const MonthChart: FC = () => {
-  return <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px]">MonthChart</ChartsBox>;
+  return (
+    <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px]">
+      <ChartHeader />
+    </ChartsBox>
+  );
 };
--- src/widgets/Charts/ui/WeeklyChart.tsx
+++ src/widgets/Charts/ui/WeeklyChart.tsx
@@ -1,8 +1,10 @@
 import { type FC } from 'react';
-import { ChartsBox } from '@/widgets/Charts/index';
+import { ChartsBox, ChartHeader } from '@/widgets/Charts/index';
 
 export const WeeklyChart: FC = () => {
   return (
-    <ChartsBox paddingClass="pt-[29px] pr-[28.74px] pb-[27px] pl-[31px]">WeeklyChart</ChartsBox>
+    <ChartsBox paddingClass="pt-[29px] pr-[28.74px] pb-[27px] pl-[31px]">
+      <ChartHeader />
+    </ChartsBox>
   );
 };
