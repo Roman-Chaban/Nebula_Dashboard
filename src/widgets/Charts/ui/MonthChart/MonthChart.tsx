a510e7343f22fdc9373479a5f6fec5d01af5f8a2
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 18:00:19 +0300

    Feat: Add MonthChart component with localized extraBlock in ChartHeader

--- src/widgets/Charts/ui/MonthChart/MonthChart.tsx
+++ src/widgets/Charts/ui/MonthChart/MonthChart.tsx
@@ -2,17 +2,18 @@
 
 import { type FC } from 'react';
 
-import { ChartsBox, ChartHeader } from '@/widgets/Charts/index';
+import { ChartsBox, ChartHeader, MonthChartMain } from '@/widgets/Charts/index';
 import { CalendarIcon } from '@/shared/ui/Icons/Icons';
 import { useTranslation } from 'react-i18next';
 
 export const MonthChart: FC = () => {
   const { t } = useTranslation();
   return (
-    <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px]">
+    <ChartsBox paddingClass="pt-[26px] pr-[32px] pb-[27px] pl-[31px] gap-[28px]">
       <ChartHeader
         extraBlock={{ label: t('CHARTS.WEEKLY_CHART.extra_title'), icon: <CalendarIcon /> }}
       />
+      <MonthChartMain />
     </ChartsBox>
   );
 };
