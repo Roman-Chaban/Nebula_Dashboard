209cbcaaa28096a09984a09026557025f6b6fd54
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-08 16:34:59 +0300

    Feat: Export ChartHeader from Charts index for improved accessibility

--- src/widgets/Charts/index.ts
+++ src/widgets/Charts/index.ts
@@ -1,3 +1,4 @@
 export { MonthChart } from '@/widgets/Charts/ui/MonthChart';
 export { WeeklyChart } from '@/widgets/Charts/ui/WeeklyChart';
 export { ChartsBox } from '@/widgets/Charts/ui/ChartsBox';
+export { ChartHeader } from '@/widgets/Charts/ui/ChartHeader';
