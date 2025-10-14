af87f811606193a85241b2bd73919a6ab92d40be
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-14 17:45:09 +0300

    Refactor: Add tooltipRef for container, and tooltipHeight state, changed them

--- src/widgets/Charts/ui/MonthChart/components/ChartTooltip.tsx
+++ src/widgets/Charts/ui/MonthChart/components/ChartTooltip.tsx
@@ -1,29 +1,41 @@
-import { type FC } from 'react';
+'use client';
+
+import { useEffect, useRef, useState, type FC } from 'react';
 
 import type { ChartTooltipProps } from '@/widgets/Charts/model/types';
 
 import { Container } from '@/shared/ui';
 
-export const ChartTooltip: FC<ChartTooltipProps> = ({ knob, value }) => (
-  <Container
-    htmlTag="div"
-    className="pointer-events-none absolute"
-    style={{
-      left: `${knob.x}px`,
-      transform: 'translate(-50%, -100%)',
-      top: `${knob.y - 28}px`,
-    }}
-  >
-    <Container
-      htmlTag="div"
-      className="inline-flex items-center justify-center rounded-lg bg-[var(--color-chart-blue-secondary)] px-3 py-1 text-sm font-semibold text-[var(--color-white)] shadow-md"
+export const ChartTooltip: FC<ChartTooltipProps> = ({ knob, value }) => {
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+
+  const [tooltipHeight, setTooltipHeight] = useState<number>(0);
+
+  useEffect(() => {
+    if (tooltipRef?.current) {
+      setTooltipHeight(tooltipRef.current.offsetHeight);
+    }
+  }, [value, knob]);
+
+  const CIRCLE_RADIUS = 6;
+  const top = knob.y - CIRCLE_RADIUS - tooltipHeight - 4;
+
+  return (
+    <div
+      className="pointer-events-none absolute"
+      style={{
+        left: `${knob.x}px`,
+        top: `${top}px`,
+        transform: 'translateX(-50%)',
+      }}
+      ref={tooltipRef}
     >
-      ${value}.00
-    </Container>
-    <Container
-      htmlTag="div"
-      className="mx-auto mt-1 h-3 w-3 rounded-full bg-[var(--color-white)]"
-      style={{ transform: 'translateY(-6px)' }}
-    />
-  </Container>
-);
+      <Container
+        htmlTag="div"
+        className="inline-flex items-center justify-center rounded-lg bg-[var(--color-chart-blue-secondary)] px-3 py-1 text-sm font-semibold text-[var(--color-white)] shadow-md"
+      >
+        ${value}.00
+      </Container>
+    </div>
+  );
+};
