638759467bff5ccfb0e85f6f5531d4b824953aa3
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-29 15:10:55 +0200

    Feat: Added new constants into Charts ui

--- src/widgets/Charts/model/constants.ts
+++ src/widgets/Charts/model/constants.ts
@@ -10,3 +10,12 @@ export const CHART_CONFIG: ChartConfig = {
   padding: { top: 32, right: 24, bottom: 56, left: 24 },
   svg: { width: 760, height: 220 },
 };
+
+export const THUMB_WIDTH = 600;
+export const THUMB_HEIGHT = 120;
+export const BOTTOM_MARGIN = 28;
+export const CHART_HEIGHT = THUMB_HEIGHT - BOTTOM_MARGIN;
+export const PADDING_X = 24;
+
+export const AVALIABLE_WIDTH = THUMB_WIDTH - PADDING_X * 2;
+export const GAP = 12;
