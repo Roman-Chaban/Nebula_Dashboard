68d852d2459e23ca17b0c2777d7da1bc978ee585
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-29 15:15:30 +0200

    Feat: add data config

--- src/widgets/Charts/lib/config.tsx
+++ src/widgets/Charts/lib/config.tsx
@@ -1,4 +1,4 @@
-import { MonthChartInformationItems } from '@/widgets/Charts/model/types';
+import { DaysData, MonthChartInformationItems } from '@/widgets/Charts/model/types';
 
 import { DoneTrackIcon } from '@/shared/ui/Icons/Icons';
 
@@ -12,5 +12,80 @@ export const monthChartInformationItems: MonthChartInformationItems = [
       title: 'On track',
       icon: <DoneTrackIcon />,
     },
+  },
+];
+
+export const data: DaysData = [
+  {
+    day: 17,
+    segments: [
+      { value: 17, color: 'var(--color-chart-primary)' },
+      { value: 18, color: 'var(--color-chart-secondary)' },
+      { value: 19, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 18,
+    segments: [
+      { value: 18, color: 'var(--color-chart-primary)' },
+      { value: 19, color: 'var(--color-chart-secondary)' },
+      { value: 20, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 19,
+    segments: [
+      { value: 19, color: 'var(--color-chart-primary)' },
+      { value: 20, color: 'var(--color-chart-secondary)' },
+      { value: 21, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 20,
+    segments: [
+      { value: 20, color: 'var(--color-chart-primary)' },
+      { value: 21, color: 'var(--color-chart-secondary)' },
+      { value: 22, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 21,
+    segments: [
+      { value: 21, color: 'var(--color-chart-primary)' },
+      { value: 22, color: 'var(--color-chart-secondary)' },
+      { value: 23, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 22,
+    segments: [
+      { value: 22, color: 'var(--color-chart-primary)' },
+      { value: 23, color: 'var(--color-chart-secondary)' },
+      { value: 24, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 23,
+    segments: [
+      { value: 23, color: 'var(--color-chart-primary)' },
+      { value: 24, color: 'var(--color-chart-secondary)' },
+      { value: 25, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 24,
+    segments: [
+      { value: 24, color: 'var(--color-chart-primary)' },
+      { value: 25, color: 'var(--color-chart-secondary)' },
+      { value: 26, color: 'var(--color-deep-blue)' },
+    ],
+  },
+  {
+    day: 25,
+    segments: [
+      { value: 25, color: 'var(--color-chart-primary)' },
+      { value: 26, color: 'var(--color-chart-secondary)' },
+      { value: 27, color: 'var(--color-deep-blue)' },
+    ],
   },
 ];
