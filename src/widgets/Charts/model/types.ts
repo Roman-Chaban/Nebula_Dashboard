08026616b6b888f18e762618491d1565d8cdda40
Roman Chaban <roman.chaban1001@gmail.com>
2025-10-30 17:25:28 +0200

    Refactor: Deleted useless types

--- src/widgets/Charts/model/types.ts
+++ src/widgets/Charts/model/types.ts
@@ -2,22 +2,6 @@ import { ReactNode } from 'react';
 
 export type ChartsProps = { children?: ReactNode };
 
-export type ChartsBoxProps = {
-  children?: ReactNode;
-  paddingClass?: string;
-};
-
-export type ChartHeaderExtraBlock = {
-  label: string;
-  icon?: ReactNode;
-};
-
-export type ChartHeaderProps = {
-  children?: ReactNode;
-  title?: string;
-  extraBlock?: ChartHeaderExtraBlock;
-};
-
 export type MonthChartInformationTrackItem = {
   title: string;
   icon: ReactNode;
