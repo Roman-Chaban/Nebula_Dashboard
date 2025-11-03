3ac8f87f9dd11c20cc7e31958d9a8520e2afeed1
Roman Chaban <roman.chaban1001@gmail.com>
2025-11-03 15:55:56 +0200

    Feat: Committed Checkbox component

--- src/widgets/CheckTable/ui/CheckTable.tsx
+++ src/widgets/CheckTable/ui/CheckTable.tsx
@@ -3,11 +3,14 @@ import { type FC } from 'react';
 import { CommonContainer } from '@/shared/ui/CommonContainer/CommonContainer';
 import { CommonHeader } from '@/shared/ui/Common/CommonHeader/CommonHeader';
 import { DotsIcon } from '@/shared/ui/Icons/Icons';
+// import { Checkbox } from '@/shared/ui/Checkbox/Checkbox';
 
 export const CheckTable: FC = () => {
   return (
     <CommonContainer maxWidth="774px" paddingClass="pt-[19px] pr-[25px] pb-[28px] pl-[31px]">
       <CommonHeader title="Check Table" icon={<DotsIcon />} />
+      {/* TODO: Temporary commented this component */}
+      {/* <Checkbox size="md" variant="primary" /> */}
     </CommonContainer>
   );
 };
