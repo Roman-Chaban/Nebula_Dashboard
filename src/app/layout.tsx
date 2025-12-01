c4d3bf88f6135d10d16532f8abe770682fdac204
Roman Chaban <roman.chaban1001@gmail.com>
2025-12-01 20:58:54 +0200

    Feat: Integrated RTK `Provider` for layout wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { DM_Sans } from 'next/font/google';
 import { Sidebar } from '@/widgets/Sidebar';
 import { Container } from '@/shared/ui/Container/Container';
 
+import { Provider } from 'react-redux';
+import { store } from '@/shared/store/store';
+
 import '@/styles/globals.css';
 
 export const dm_sans = DM_Sans({
@@ -18,10 +21,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-theme="light">
       <body className={`${dm_sans.className}`}>
-        <Container className="mx-auto flex w-full max-w-[1920px]">
-          <Sidebar />
-          {children}
-        </Container>
+        <Provider store={store}>
+          <Container className="mx-auto flex w-full max-w-[1920px]">
+            <Sidebar />
+            {children}
+          </Container>
+        </Provider>
       </body>
     </html>
   );
