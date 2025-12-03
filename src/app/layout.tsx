fcc1bbcb3b0084eadbfd6d0221eddd5c31d1d321
Roman Chaban <roman.chaban1001@gmail.com>
2025-12-03 18:17:20 +0200

    Feat: Deleted useless Provider, moved it into `GlobalProviders`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,6 @@ import { DM_Sans } from 'next/font/google';
 import { Sidebar } from '@/widgets/Sidebar';
 import { Container } from '@/shared/ui/Container/Container';
 
-import { Provider } from 'react-redux';
-import { store } from '@/shared/store/store';
-
 import '@/styles/globals.css';
 
 export const dm_sans = DM_Sans({
@@ -21,12 +18,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" data-theme="light">
       <body className={`${dm_sans.className}`}>
-        <Provider store={store}>
-          <Container className="mx-auto flex w-full max-w-[1920px]">
-            <Sidebar />
-            {children}
-          </Container>
-        </Provider>
+        <Container className="mx-auto flex w-full max-w-[1920px]">
+          <Sidebar />
+          {children}
+        </Container>
       </body>
     </html>
   );
